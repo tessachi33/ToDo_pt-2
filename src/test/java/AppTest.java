427d0e045e505396497b3f5b9f00f39378e0c6f2
jocelynalsdorf <jfrodsla@gmail.com>
2015-08-14 07:56:45 -0700

    base setup for quick spark app build and deploy

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,33 @@
+import org.fluentlenium.adapter.FluentTest;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AppTest extends FluentTest{
+  public WebDriver webDriver = new HtmlUnitDriver();
+  public WebDriver getDefaultDriver(){
+    return webDriver;
+  }
+
+  @ClassRule
+  public static ServerRule server = new ServerRule();
+
+  // @Test
+  // public void rootTest(){
+  //   goTo("http://localhost:4567/");
+  //   assertThat(pageSource()).contains("");
+  // }
+  
+  // @Test
+  // public void getchangedword(){
+  //   goTo("http://localhost:4567/");
+  //   fill("#").with("");
+  //   fill("#").with("");
+  //   submit(".btn");
+  //   assertThat(pageSource()).contains("");
+  // }
+
+}
