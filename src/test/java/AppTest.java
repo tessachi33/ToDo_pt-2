027d0df8b542bbda190a23e1e7c70152594fbf78
teresa fletcher & jocelyn alsdorf <student@epicodus.com>
2015-08-20 11:07:48 -0700

    Passed 2 integration tests

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -33,12 +33,12 @@ public void categoryIsCreatedTest() {
   assertThat(pageSource()).contains("Your category has been saved.");
 }
 
-// @Test
-// public void categoryIsDisplayedTest() {
-//   goTo("http://localhost:4567/categories/new");
-//   fill("#name").with("Household chores");
-//   submit(".btn");
-//   click("a", withText("View categories"));
-//   assertThat(pageSource()).contains("Household chores");
-// }
+@Test
+public void categoryIsDisplayedTest() {
+  goTo("http://localhost:4567/categories/new");
+  fill("#name").with("Household chores");
+  submit(".btn");
+  click("a", withText("View categories"));
+  assertThat(pageSource()).contains("Household chores");
+}
 }
